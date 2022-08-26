2d3607c99a40c2466f19da698d4fc946fb3c5638
hardikpnsp <hardikpnsp@gmail.com>
2022-08-26 15:25:41 +0530

    ENH: read file as vec of string to make modifying easier

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-use std::io::{stdin, stdout, Read, Write};
-use std::{env, fs};
-use termion::color;
+use std::io::{stdin, stdout, Read, Write, BufRead, BufReader, Lines};
+use std::{env, io};
+use std::fs::File;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let filename = args.get(1).expect("file name not provided");
-    let buffer = fs::read_to_string(filename).expect("could not read the file content");
+
+    let file = File::open(filename).expect(&format!("could not open file: {}", filename));
 
     let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut buffer: Vec<String> = vec![];
+
+    for line in io::BufReader::new(file).lines() {
+        let line = line.expect("failed reading line");
+        buffer.push(line);
+    }
 
     write!(stdout, "{}", termion::clear::All).unwrap();
-    write!(stdout, "{}", buffer).unwrap();
+    for line in buffer {
+        write!(stdout, "{}\r\n", line).unwrap();
+    }
     write!(stdout, "{}", termion::cursor::Show).unwrap();
     stdout.flush().unwrap();
 
